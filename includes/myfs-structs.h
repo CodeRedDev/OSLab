cb5f89e0d2fa27e98f740cfab3754bdaf1f0077d
Mika2147 <mika.auer@icloud.com>
2019-04-07 16:34:48 +0200

    Superblock and fileInfo

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -6,8 +6,12 @@
 //  Copyright © 2017 Oliver Waldhorst. All rights reserved.
 //
 
+
 #ifndef myfs_structs_h
 #define myfs_structs_h
+
+#include <stdlib.h>
+#include <string>
 
 #define NAME_LENGTH 255
 #define BLOCK_SIZE 512
@@ -15,5 +19,43 @@
 #define NUM_OPEN_FILES 64
 
 // TODO: Add structures of your file system here
+
+
+typedef struct{
+
+    size_t fsSize;
+    size_t freeSpace;
+    size_t maxSpace;
+
+    uint16_t dmapStart;
+    size_t dmapSize;
+
+    uint16_t fatStart;
+    size_t fatSize;
+
+    uint16_t rootStart;
+    size_t rootSize;
+
+    uint16_t dataStart;
+    size_t dataSize;
+
+}SuperBlock;
+
+typedef struct{
+
+    std::string name;
+    size_t size;
+
+    uid_t userID;
+    gid_t groupID;
+
+    time_t lastAccess
+    time_t lastChange;
+
+    uint16_t firstBlock;
+
+    mode_t readWriteExecuteRighs;
+
+}fileInfo;
 
 #endif /* myfs_structs_h */
